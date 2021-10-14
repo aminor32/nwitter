0e7f503b004b642aec1098773f1acac98e32bf2f
Donghee John <aminor32dh@gmail.com>
2021-10-15 08:19:42 +0900

    #4.0

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -28,9 +28,20 @@ const Home = ({ userObj }) => {
     };
     const onChange = (event) => {
         const {
-            target:{value},
+            target:{ value },
         } = event;
         setNweet(value)
+    };
+    const onFileChange = (event) => {
+        const {
+            target:{ files },
+        } = event;
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            console.log(finishedEvent);
+        };
+        reader.readAsDataURL(theFile);
     };
     return (
         <div>
@@ -42,6 +53,7 @@ const Home = ({ userObj }) => {
                     placeholder="What's on your mind?" 
                     maxLength={120} 
                 />
+                <input type="file" accept="image/*" onChange={onFileChange} />
                 <input type="submit" value="Nweet" />
             </form>
             <div>
