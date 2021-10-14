580d634701bd6485f790dd780150fa9d82eb989c
Donghee John <aminor32dh@gmail.com>
2021-10-15 08:39:23 +0900

    $4.1

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -7,7 +7,6 @@ const Nweet = ({ nweetObj, isOwner }) => {
     const [newNweet, setNewNweet] = useState(nweetObj.text);
     const onDeleteClick = async () => {
         const ok = window.confirm("Are you sure you want to delete this nweet?");
-        console.log(ok);
         if (ok) {
             deleteDoc(doc(dbService, `nweets/${nweetObj.id}`));
         }
