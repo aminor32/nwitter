c5321389e9bb4a51ec6e06868ec23c19432b948b
Donghee John <aminor32dh@gmail.com>
2021-10-15 21:00:10 +0900

    #4.4

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,6 +1,7 @@
-import { dbService } from "fbase";
+import { dbService, storageService } from "fbase";
 import React, { useState }from "react";
 import { doc, deleteDoc, updateDoc } from "firebase/firestore";
+import { ref, deleteObject } from "firebase/storage";
 
 const Nweet = ({ nweetObj, isOwner }) => {
     const [editing, setEditing] = useState(false);
@@ -9,6 +10,7 @@ const Nweet = ({ nweetObj, isOwner }) => {
         const ok = window.confirm("Are you sure you want to delete this nweet?");
         if (ok) {
             deleteDoc(doc(dbService, `nweets/${nweetObj.id}`));
+            await deleteObject(ref(storageService, nweetObj.attachmentUrl));
         }
     };
     const toggleEditing = () => setEditing((prev) => !prev);
