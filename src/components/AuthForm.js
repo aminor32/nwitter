f76902e8cedd31414c1aabe05cef6f4f11e7fb8d
Donghee John <aminor32dh@gmail.com>
2021-10-17 22:27:42 +0900

    #6.1

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -30,7 +30,7 @@ const AuthForm = () => {
     const toggleAccount = () => setNewAccount(prev => !prev);
     return (
         <>
-            <form onSubmit={onSubmit}>
+            <form onSubmit={onSubmit} className="container">
                 <input
                     name="email"
                     type="email"
@@ -38,6 +38,7 @@ const AuthForm = () => {
                     required
                     value={email}
                     onChange={onChange}
+                    className="authInput"
                 />
                 <input
                     name="password"
@@ -46,14 +47,16 @@ const AuthForm = () => {
                     required
                     value={password}
                     onChange={onChange}
+                    className="authInput"
                 />
                 <input
                     type="submit"
+                    className="authInput authSubmit"
                     value={newAccount ? "Create Account" : "Sign In"}
                 />
-                {error}
+                {error && <span className="authError">{error}</span>}
             </form>
-            <span onClick={toggleAccount}>
+            <span onClick={toggleAccount} className="authSwitch">
                 {newAccount ? "Sign In" : "Create Account"}
             </span>
         </>
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -2,6 +2,8 @@ import React from "react";
 import { authService } from "fbase";
 import { signInWithPopup, GithubAuthProvider, GoogleAuthProvider } from "firebase/auth";
 import AuthForm from "components/AuthForm";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter, faGoogle, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const Auth = () => {
     const onSocialClick = async (event) => {
@@ -12,19 +14,26 @@ const Auth = () => {
         } else if (name === "github") {
             provider = new GithubAuthProvider();
         }
-        const data = await signInWithPopup(authService,provider);
-        console.log(data);
+        const data = await signInWithPopup(authService, provider);
     };
     return(
         <div>
-            <AuthForm />
-            <div>
-                <button onClick={onSocialClick} name="google">
-                    Continue with Google
-                </button>
-                <button onClick={onSocialClick} name="github">
-                    Continue with GitHub
-                </button>
+            <div className="authContainer">
+                <FontAwesomeIcon
+                    icon={faTwitter}
+                    color={"#04AAFF"}
+                    size="3x"
+                    style={{ marginBottom: 30 }}
+                />
+                <AuthForm />
+                <div className="authBtns">
+                    <button onClick={onSocialClick} name="google" className="authBtn">
+                        Continue with Google <FontAwesomeIcon icon={faGoogle} />
+                    </button>
+                    <button onClick={onSocialClick} name="github" className="authBtn">
+                        Continue with GitHub <FontAwesomeIcon icon={faGithub} />
+                    </button>
+                </div>
             </div>
         </div>
     );
